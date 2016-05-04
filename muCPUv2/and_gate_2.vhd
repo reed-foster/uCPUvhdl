c6222fdbf7fc770e1184720ff5d54266f48cfb6a
reed-foster <reed.a.foster@gmail.com>
2016-05-04 09:08:51 -0700

    Update and_gate_2.vhd

--- muCPUv2/and_gate_2.vhd
+++ muCPUv2/and_gate_2.vhd
@@ -16,12 +16,12 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity and_gate_2 is
-	port (a : in  std_logic;
-			b : in  std_logic;
-			p : out  std_logic);
+   port (a : in  std_logic;
+         b : in  std_logic;
+         p : out std_logic);
 end and_gate_2;
 
 architecture dataflow of and_gate_2 is
 begin
-	p <= a and b;
+   p <= a and b;
 end dataflow;
