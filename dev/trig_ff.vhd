cc97e8268c0cf7f578817c76027033de67b80847
reed-foster <reed.a.foster@gmail.com>
2016-05-04 12:03:37 -0700

    Update trig_ff.vhd

--- dev/trig_ff.vhd
+++ dev/trig_ff.vhd
@@ -15,23 +15,23 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity trig_ff is
-	port (trig_in	: in	std_logic;
-			trig_out	: out	std_logic;
-			clk		: in	std_logic);
+   port (trig_in	: in	std_logic;
+         trig_out	: out	std_logic;
+         clk		: in	std_logic);
 end trig_ff;
 
 architecture behavioral of trig_ff is
    signal trig_out_tmp : std_logic := '0';
 begin
 	
-	process (trig_in, clk)
-	begin
-		if (clk = '1') then
-			trig_out_tmp <= '0';
-		elsif rising_edge(trig_in) then
-			trig_out_tmp <= '1';
-		end if;
-	end process;
+   process (trig_in, clk)
+   begin
+      if (clk = '1') then
+         trig_out_tmp <= '0';
+      elsif rising_edge(trig_in) then
+         trig_out_tmp <= '1';
+      end if;
+   end process;
    
    trig_out <= trig_out_tmp;
 
