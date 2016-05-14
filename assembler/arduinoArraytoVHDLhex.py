35ef20195b410c6e8e64b2d50a64021fee1928b8
reed-foster <reed.a.foster@gmail.com>
2016-05-14 16:52:19 -0700

    Update arduinoArraytoVHDLhex.py
    changed formatting of output string to make output hex array more dense

--- assembler/arduinoArraytoVHDLhex.py
+++ assembler/arduinoArraytoVHDLhex.py
@@ -1,8 +1,26 @@
 import re
 
 def formatArray(arraystrin):
-    array = re.split(", |,\n", arraystrin)
+    array = re.split("  |, |,\n", arraystrin)
     str_out = ""
     for i in range(len(array)):
-        str_out += ("x\"" + str.lower(array[i][2:]) + "\", ")
+        #print(array[i])
+        if ((i%16) == 15):
+            if (i > 999):
+                str_out += (str(i) + " => x\"" + str.lower(array[i][2:]) + "\",\n")
+            elif (i > 99):
+                str_out += (str(i) + "  => x\"" + str.lower(array[i][2:]) + "\",\n")
+            elif (i > 9):
+                str_out += (str(i) + "   => x\"" + str.lower(array[i][2:]) + "\",\n")
+            else:
+                str_out += (str(i) + "    => x\"" + str.lower(array[i][2:]) + "\",\n")
+        else:
+            if (i > 999):
+                str_out += (str(i) + " => x\"" + str.lower(array[i][2:]) + "\", ")
+            elif (i > 99):
+                str_out += (str(i) + "  => x\"" + str.lower(array[i][2:]) + "\", ")
+            elif (i > 9):
+                str_out += (str(i) + "   => x\"" + str.lower(array[i][2:]) + "\", ")
+            else:
+                str_out += (str(i) + "    => x\"" + str.lower(array[i][2:]) + "\", ")
     print str_out
