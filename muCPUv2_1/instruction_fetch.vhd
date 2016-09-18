04aa805efd033410183d2536e20d6e4570e55702
reed-foster <reed.a.foster@gmail.com>
2016-09-17 23:04:27 -0700

    Update instruction_fetch.vhd

--- muCPUv2_1/instruction_fetch.vhd
+++ muCPUv2_1/instruction_fetch.vhd
@@ -16,69 +16,69 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity instruction_fetch is
-	generic (romstartaddr : natural := 128);
-	port (clk				: in std_logic;
-			ringct0			: in std_logic;
-			ringct1			: in std_logic;
-			ringct2			: in std_logic;
-			pc_in				: in std_logic_vector (15 downto 0);
-			instr_byte_ser	: in std_logic_vector (7 downto 0);
-			instr_word_out	: out std_logic_vector (15 downto 0);
-			pc_out			: out std_logic_vector (15 downto 0));
+   generic (romstartaddr : natural := 128);
+   port (clk : in std_logic;
+         ringct0        : in std_logic;
+         ringct1        : in std_logic;
+         ringct2        : in std_logic;
+         pc_in          : in std_logic_vector (15 downto 0);
+         instr_byte_ser : in std_logic_vector (7 downto 0);
+         instr_word_out	: out std_logic_vector (15 downto 0);
+         pc_out         : out std_logic_vector (15 downto 0));
 end instruction_fetch;
 
 architecture structural of instruction_fetch is
-	signal reg_we			: std_logic := '0';
-	signal ishift_we		: std_logic := '0';
-	signal pcmux_out		: std_logic_vector (15 downto 0) := (others => '0');
-	signal pcinc_out		: std_logic_vector (15 downto 0) := (others => '0');
-	signal pcout_tmp		: std_logic_vector (15 downto 0) := (others => '0');
+   signal reg_we    : std_logic := '0';
+   signal ishift_we : std_logic := '0';
+   signal pcmux_out : std_logic_vector (15 downto 0) := (others => '0');
+   signal pcinc_out : std_logic_vector (15 downto 0) := (others => '0');
+   signal pcout_tmp : std_logic_vector (15 downto 0) := (others => '0');
 	
-	component instructionshifter is
-		port (shift_in 	: in  std_logic_vector (7 downto 0);
-				clk 			: in  std_logic;
-				en 			: in  std_logic;
-				shift_out	: out  std_logic_vector (15 downto 0));
-	end component;
-	
-	component adder is
-		generic(dwidth : positive);
-		port (a : in  std_logic_vector (dwidth-1 downto 0);
-				b : in  std_logic_vector (dwidth-1 downto 0) := (dwidth-1 downto 1 => '0') & '1';
-				s : out  std_logic_vector (dwidth-1 downto 0));
-	end component;
-	
-	component reg is
-		generic(dwidth : positive;
-				  q_init : natural);
-		port (D		: in  std_logic_vector (dwidth-1 downto 0);
-				en		: in  std_logic;
-				clk	: in  std_logic;
-				clr	: in  std_logic := '0';
-				Q		: out  std_logic_vector (dwidth-1 downto 0));
-	end component;
-	
-	component or_gate_2 is
-		port (a : in  std_logic;
-				b : in  std_logic;
-				s : out  std_logic);
-	end component;
-	
-	component or_gate_3 is
-		port (a : in  std_logic;
-				b : in  std_logic;
-				c : in  std_logic;
-				s : out  std_logic);
-	end component;
-	
-	component mux is
-		generic (dwidth : positive);
-		port (data_in_0	: in std_logic_vector(dwidth-1 downto 0);
-				data_in_1	: in std_logic_vector(dwidth-1 downto 0);
-				sel			: in std_logic;
-				data_out		: out std_logic_vector(dwidth-1 downto 0));
-	end component;
-	
+   component instructionshifter is
+      port (shift_in  : in  std_logic_vector (7 downto 0);
+            clk       : in  std_logic;
+            en        : in  std_logic;
+            shift_out : out std_logic_vector (15 downto 0));
+      end component;
+      
+      component adder is
+         generic(dwidth : positive);
+         port (a : in  std_logic_vector (dwidth-1 downto 0);
+               b : in  std_logic_vector (dwidth-1 downto 0) := (dwidth-1 downto 1 => '0') & '1';
+               s : out std_logic_vector (dwidth-1 downto 0));
+      end component;
+      
+      component reg is
+         generic(dwidth : positive;
+                 q_init : natural);
+         port (D   : in  std_logic_vector (dwidth-1 downto 0);
+               en  : in  std_logic;
+               clk : in  std_logic;
+               clr : in  std_logic := '0';
+               Q   : out std_logic_vector (dwidth-1 downto 0));
+      end component;
+         
+      component or_gate_2 is
+         port (a : in  std_logic;
+               b : in  std_logic;
+               s : out std_logic);
+      end component;
+   
+      component or_gate_3 is
+         port (a : in  std_logic;
+               b : in  std_logic;
+               c : in  std_logic;
+               s : out  std_logic);
+      end component;
+      
+      component mux is
+         generic (dwidth : positive);
+            port (data_in_0 : in std_logic_vector(dwidth-1 downto 0);
+                  data_in_1 : in std_logic_vector(dwidth-1 downto 0);
+                  sel       : in std_logic;
+                  data_out  : out std_logic_vector(dwidth-1 downto 0));
+      end component;
+      
 begin
 	--multiplexer for pc
 	pcmux0			: mux 	generic map(dwidth 		=> 16)
